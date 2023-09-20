93869c7ce2a622cbf012d606d1bae752f7f9267a
Gabriel Kozaczuk <mylitle43@gmail.com>
2023-09-20 17:36:52 +0200

    Make round system for global

--- public/js/global_scripts.js
+++ public/js/global_scripts.js
@@ -73,38 +73,77 @@ const choice2 = document.querySelector("#choice2")
 const choice3 = document.querySelector("#choice3")
 const choice4 = document.querySelector("#choice4")
 const flag = document.querySelector("#flag")
+    
+let round = (position) => {
+    let prefix = prefixes[Math.floor(Math.random() * prefixes.length)]
+    console.log(prefix)
+    flag.innerHTML = `<img src="https://flagsapi.com/${prefix}/flat/64.png">`
 
-let prefix = prefixes[Math.floor(Math.random() * prefixes.length)]
-console.log(prefix)
-flag.innerHTML = `<img src="https://flagsapi.com/${prefix}/flat/64.png">`
-
-correctPosition = Math.floor(Math.random() * 4)
-
-switch(correctPosition){
-    case 1 :
-        choice1.textContent = countries[prefixes.indexOf(prefix)]
-        choice2.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice3.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice4.textContent = countries[Math.floor(Math.random() * countries.length)]
-        break
-    case 2 :
-        choice2.textContent = countries[prefixes.indexOf(prefix)]
-        choice1.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice3.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice4.textContent = countries[Math.floor(Math.random() * countries.length)]
-        break
-    case 3 :
-        choice3.textContent = countries[prefixes.indexOf(prefix)]
-        choice2.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice1.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice4.textContent = countries[Math.floor(Math.random() * countries.length)]
-        break
-    case 4 :
-        choice4.textContent = countries[prefixes.indexOf(prefix)]
-        choice2.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice3.textContent = countries[Math.floor(Math.random() * countries.length)]
-        choice1.textContent = countries[Math.floor(Math.random() * countries.length)]
-        break
-    default:
-        console.log("Error")
+    switch(position){
+        case 1 :
+            choice1.textContent = countries[prefixes.indexOf(prefix)]
+            choice2.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice3.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice4.textContent = countries[Math.floor(Math.random() * countries.length)]
+            break
+        case 2 :
+            choice2.textContent = countries[prefixes.indexOf(prefix)]
+            choice1.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice3.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice4.textContent = countries[Math.floor(Math.random() * countries.length)]
+            break
+        case 3 :
+            choice3.textContent = countries[prefixes.indexOf(prefix)]
+            choice2.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice1.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice4.textContent = countries[Math.floor(Math.random() * countries.length)]
+            break
+        case 4 :
+            choice4.textContent = countries[prefixes.indexOf(prefix)]
+            choice2.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice3.textContent = countries[Math.floor(Math.random() * countries.length)]
+            choice1.textContent = countries[Math.floor(Math.random() * countries.length)]
+            break
+        default:
+            console.log("Error")
+    }
 }
+
+let correctPosition = Math.floor(Math.random() * 3) + 1
+let chosenPosition = 0
+
+round(correctPosition)
+
+choice1.addEventListener('click', () => {
+    chosenPosition = 1
+    if(chosenPosition == correctPosition){
+        correctPosition = Math.floor(Math.random() * 3) + 1
+        round(correctPosition)
+    }
+})
+
+choice2.addEventListener('click', () => {
+    chosenPosition = 2
+    if(chosenPosition == correctPosition){
+        correctPosition = Math.floor(Math.random() * 3) + 1
+        round(correctPosition)
+    }
+})
+
+choice3.addEventListener('click', () => {
+    chosenPosition = 3
+    if(chosenPosition == correctPosition){
+        correctPosition = Math.floor(Math.random() * 3) + 1
+        round(correctPosition)
+    }
+})
+
+choice4.addEventListener('click', () => {
+    chosenPosition = 4
+    if(chosenPosition == correctPosition){
+        correctPosition = Math.floor(Math.random() * 3) + 1
+        round(correctPosition)
+    }
+})
+
+console.log(correctPosition)
